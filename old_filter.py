b59b9f79ce1569e1071ee7723377c5c41b150084
flexiG17 <mister23krut@gmail.com>
2021-12-06 17:00:55 +0500

    update fresh version of old_version

--- old_filter.py
+++ old_filter.py
@@ -1,45 +1,30 @@
 from PIL import Image
 import numpy as np
 
-def pixel_filter(side: int, step: int):
-    length, height = len(img_arr), len(img_arr[1])
-    x = 0
-    while x < length:
-        y = 0
-        while y < height:
-            result = 0
-            for n, m in np.nditer([np.arange(x, x + side)[:, None], np.arange(y, y + side)]):
-                result += img_arr[n][m][0] / 3 + img_arr[n][m][1] / 3 + img_arr[n][m][2] / 3
-            avg_brightness = int(result) // (side * side)
-            pixel_coloring(avg_brightness, x, y, side, step)
-            y += side
-        x += side
-
-
-def pixel_coloring(x, y, side, step):
-    result = 0
-    for n, m in np.nditer([np.arange(x, x + side)[:, None], np.arange(y, y + side)]):
-        result += img_arr[n][m][0] / 3 + img_arr[n][m][1] / 3 + img_arr[n][m][2] / 3
-    avg_brightness = int(result) // (side * side)
-    for n in range(x, x + side):
-        for m in range(y, y + side):
-            img_arr[n][m][range(3)] = int(avg_brightness // step) * step
-
-def start_program():
-    global img_arr
-    print("Введите имя исходного изображения:")
-    original_img_name = input()
-    print("Введите имя конечного изображения:")
-    filtered_img_name = input()
-    print("Задайте значения размера мозайки и шага яркости через пробел:")
-    mosaic_side, step = map(int, input().split())
-    print("Обработка изображения началась, ожидайте")
-    img_arr = np.array(Image.open(original_img_name))
-    pixel_filter(mosaic_side, step)
-    changed_img = Image.fromarray(img_arr)
-    changed_img.save(filtered_img_name)
-    print("Обработка успешно завершена")
-
-
 np.seterr(over='ignore')
-start_program()
+arr = np.array(Image.open("img2.jpg"))
+a, a1 = len(arr), len(arr[1])
+i = 0
+
+while i < a:
+    j = 0
+    while j < a1:
+        s = 0
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                n1 = arr[x][y][0] / 3
+                n2 = arr[x][y][1] / 3
+                n3 = arr[x][y][2] / 3
+                M = n1 + n2 + n3
+                s += M
+        s = int(s // 100)
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                arr[x][y][0] = int(s // 50) * 50
+                arr[x][y][1] = int(s // 50) * 50
+                arr[x][y][2] = int(s // 50) * 50
+        j += 10
+    i += 10
+
+res = Image.fromarray(arr)
+res.save('res.jpg')
